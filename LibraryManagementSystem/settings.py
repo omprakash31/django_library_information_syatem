c74b654b7476393efaf4d4e29712f491b7e07253
omprakash31 <72689654+omprakash31@users.noreply.github.com>
2022-08-07 11:11:48 +0530

    Update settings.py

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/templates/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
