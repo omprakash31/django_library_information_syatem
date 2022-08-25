d40325be1e00e1368cf36c29cdedce65ebbcc910
omprakash31 <72689654+omprakash31@users.noreply.github.com>
2022-08-25 12:05:56 +0530

    Update updater.py

--- library/updater.py
+++ library/updater.py
@@ -1,11 +1,26 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+from .models import *
 def tick():
-    print('One tick!')
+    r=reserved_book.objects.all()
+    for rbook in r:
+        # rbook.
+        r=(datetime.today().date()-rbook.reserve_date).days
+        if r>=7:
+            available_books.objects.create(title=rbook.title,ISBN=rbook.ISBN)
+            book_added=book.objects.get(title=rbook.title)
+            book_added.copies+=1
+            book_added.save()
+            reserved_book.objects.get(ISBN=rbook.ISBN).delete()
+        print(rbook.user_id,datetime.now())
 
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds=10)
+    # scheduler.add_job(tick, 'interval', seconds=10)
     # scheduler.add_job(tick, 'interval', minutes=5)
+    # scheduler.add_job(tick, 'cron', hour='*')
+    scheduler.add_job(tick, 'cron', hour=12, minute=1)
+    # scheduler.add_job(tick, 'cron', hour=12, minute=3)
+    # scheduler.add_job(tick, 'cron', hour="11.57")
     scheduler.start()
